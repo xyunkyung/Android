27d6da9f37518a7c58256e333352478c2e28f9b4
xyunkyung <50695960+xyunkyung@users.noreply.github.com>
2020-09-06 01:05:20 +0900

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,31 @@
+<?php
+    $con = mysqli_connect("localhost", "ftp아이디", "ftp비밀번호", "ftp아이디");
+    mysqli_query($con,'SET NAMES utf8');
+
+    $UserEmail = $_POST["UserEmail"];
+    $UserPwd = $_POST["UserPwd"];
+    
+    
+    $statement = mysqli_prepare($con, "SELECT * FROM USER WHERE UserEmail = ? AND UserPwd = ?");
+    mysqli_stmt_bind_param($statement, "ss", $UserEmail, $UserPwd);
+    mysqli_stmt_execute($statement);
+
+
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement, $UserEmail, $UserPwd, $UserName);
+
+    $response = array();
+    $response["success"] = false;
+    
+    while(mysqli_stmt_fetch($statement)) {
+        $response["success"] = true;
+        $response["UserEMail"] = $UserEmail;
+        $response["UserPwd"] = $UserPwd;
+        $response["UserName"] = $UserName;
+    }
+
+    echo json_encode($response);
+
+
+
+?>
